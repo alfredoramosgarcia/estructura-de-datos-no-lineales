337ed657c12cdfd683be573c9dda9d4ad1b48751
alfredoramosgarcia <alfredo.ramosgarcia@alum.uca.es>
2023-06-17 19:26:40 +0200

    iche

---
+++ Practicas Grafos/Practica 6/Practica6.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include "../Material/grafoPMC.h"
+//EJERCICIO 1
+
+template <typename tCoste>
+vector<tCoste> DijkstraInv (const GrafoP<tCoste> &G,typename GrafoP<tCoste>::vertice origen,vector<typename GrafoP<tCoste>::vertice>& P){
+
+}
